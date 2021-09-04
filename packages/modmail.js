1ef42f6d0b04dce3e6747b84c885e0d0c6d9d337
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-09-04 18:16:31 +0800

    Create modmail.js

---
+++ packages/modmail.js
@@ -0,0 +1,7 @@
+{
+ "package_name": "modmail.js",
+ "description": "A Modmail Bot Template",
+ "version": "1.0.0",
+ "repoURL": "https://github.com/botstudios/modmail.js",
+ "cloneURL": "https://github.com/botstudios/modmail.js.git"
+}
