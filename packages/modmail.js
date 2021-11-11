8d0c8b4e7818e26d6fd4bdd4a2441f6f24e715dc
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-11 22:10:19 +0800

    Example Config File

--- packages/modmail.js
+++
@@ -1,7 +0,0 @@
-{
- "package_name": "modmail.js",
- "description": "A Modmail Bot Template",
- "version": "1.0.0",
- "repoURL": "https://github.com/botstudios/modmail.js",
- "cloneURL": "https://github.com/botstudios/modmail.js.git"
-}
